d8c5d3661cefb8fc0ef844637cede210c0260741
NaraPG <naragamez1@gmail.com>
2021-06-25 12:38:35 -0500

    Primeras modificaciones

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    remoteDB: process.env.REMOTE_DB || false,
     api: {
         port: process.env.API_PORT || 3000,
     },
@@ -11,11 +12,20 @@ module.exports = {
     mysql: {
         host: process.env.MYSQL_HOST || 'remotemysql.com',
         user: process.env.MYSQL_USER || 'vT3mcxyv7L',
-        password: process.env.MYSQL_PASS || '23GZrs8KQe',
+        password: process.env.MYSQL_PASS || 'KxlIBBk4aQ',
         database: process.env.MYSQL_DB || 'vT3mcxyv7L',
     },
     mysqlService: {
         host: process.env.MYSQL_SRV_HOST || 'localhost',
         port: process.env.MYSQL_SRV_PORT || 3001,
+    },
+    cacheService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3003,
+    },
+    redis: {
+        host: process.env.REDIS_HOST || 'redis-16938.c275.us-east-1-4.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT || 16938,
+        password: process.env.REDIS_PASS || 'Gm3HcJY71JRYYUvlMCUTs5LDRIZ2YAT3',
     }
 }
